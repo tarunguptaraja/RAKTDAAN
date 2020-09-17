d90119b285cc46f2f4fa65d4399371c6e0aae2ee
tarunguptaraja <the.tarun.gupta.raja@gmail.com>
2020-09-17 22:11:47 +0530

    Update

---
+++ app/src/main/java/com/tarunguptaraja/raktdaan/adapters/RequestAdaptor.java
@@ -0,0 +1,67 @@
+package com.tarunguptaraja.raktdaan.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.PermissionChecker;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.tarunguptaraja.raktdaan.R;
+import com.tarunguptaraja.raktdaan.dataModels.RequestDataModel;
+
+import java.util.List;
+
+public class RequestAdaptor extends RecyclerView.Adapter<RequestAdaptor.ViewHolder> {
+    private List<RequestDataModel> dataSet;
+    private Context context;
+
+    public RequestAdaptor(
+            List<RequestDataModel> dataSet, Context context) {
+        this.dataSet = dataSet;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.request_item_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder,
+                                 final int position) {
+        holder.message.setText(dataSet.get(position).getMessage());
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return dataSet.size();
+    }
+
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView message;
+        ImageView imageView, callButton, shareButton;
+
+        ViewHolder(final View itemView) {
+            super(itemView);
+            message = itemView.findViewById(R.id.message);
+            imageView = itemView.findViewById(R.id.image);
+            callButton = itemView.findViewById(R.id.call_button);
+            shareButton = itemView.findViewById(R.id.share_button);
+        }
+
+    }
+}
